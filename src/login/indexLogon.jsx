d3f9fc1a6288499df0134d7c625e7d8ec6abb1c2
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-13 19:10:50 -0300

    body feito

--- src/login/indexLogon.jsx
+++ src/login/indexLogon.jsx
@@ -5,6 +5,27 @@ export default function Login() {
     <>
       <div className={styles.login}>
         <h1>Login</h1>
+        <form action="#">
+          <div className={styles.caixaUsuario}>
+            <input type="text" placeholder="Nome de usuario" required />
+          </div>
+          <div className={styles.caixaSenha}>
+            <input type="password" placeholder="Senha" required />
+          </div>
+          <div className={styles.caixaDown}>
+            <label type="text">
+              <input type="checkbox" />
+              Lembrar
+            </label>
+            <a href="#">Esqueci minha senha</a>
+          </div>
+          <button type="submit">Entrar</button>
+          <div className={styles.aqui}>
+            <p>
+              Não tenho conta?<a href="#">Cadatre AQUI!</a>
+            </p>
+          </div>
+        </form>
       </div>
     </>
   );

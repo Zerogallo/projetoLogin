96e92bb6d98f730e6b04310902bc8591933ab2b5
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-13 20:25:47 -0300

    melhoria

--- src/login/indexLogon.jsx
+++ src/login/indexLogon.jsx
@@ -3,27 +3,27 @@ import styles from "./stylesLogin.module.css";
 export default function Login() {
   return (
     <>
-      <div className={styles.login}>
-        <h1>Login</h1>
-        <form action="#">
-          <div className={styles.caixaUsuario}>
+      <div className={styles.container}>
+        <div className={styles.herader}>
+          <h1>Login</h1>
+        </div>
+        <form className={styles.loginForm}>
+          <div className={styles.group}>
             <input type="text" placeholder="Nome de usuario" required />
           </div>
-          <div className={styles.caixaSenha}>
+          <div className={styles.group}>
             <input type="password" placeholder="Senha" required />
           </div>
-          <div className={styles.caixaDown}>
-            <label type="text">
-              <input type="checkbox" />
-              Lembrar
-            </label>
-            <a href="#">Esqueci minha senha</a>
+          <div className={styles.remem}>
+            <input type="checkbox" id="recordarme" />
+            <label htmlFor="remember">Recordarme</label>
           </div>
-          <button type="submit">Entrar</button>
-          <div className={styles.aqui}>
-            <p>
-              Não tenho conta?<a href="#">Cadatre AQUI!</a>
-            </p>
+          <button type="submit" className={styles.submiButton}>
+            Entrar
+          </button>
+          <div className={styles.links}>
+            <a href="">Não tenho conta?</a>
+            <a href="">Cadatre AQUI!</a>
           </div>
         </form>
       </div>

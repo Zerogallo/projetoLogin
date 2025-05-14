34a72f57accb95fd7ab645c6ec678ad2e9cf0643
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-13 22:31:40 -0300

    icon

--- src/login/indexLogon.jsx
+++ src/login/indexLogon.jsx
@@ -1,5 +1,7 @@
 import styles from "./stylesLogin.module.css";
-
+import { FaUserCircle } from "react-icons/fa";
+import { FaKey } from "react-icons/fa6";
+import { IoEnter } from "react-icons/io5";
 export default function Login() {
   return (
     <>
@@ -9,17 +11,20 @@ export default function Login() {
         </div>
         <form className={styles.loginForm}>
           <div className={styles.group}>
+            <FaUserCircle />
             <input type="text" placeholder="Nome de usuario" required />
           </div>
           <div className={styles.group}>
+            <FaKey />
             <input type="password" placeholder="Senha" required />
           </div>
           <div className={styles.remem}>
             <input type="checkbox" id="recordarme" />
-            <label htmlFor="remember">Recordarme</label>
+            <label htmlFor="remember">Lembrar-me</label>
           </div>
           <button type="submit" className={styles.submiButton}>
             Entrar
+            <IoEnter />
           </button>
           <div className={styles.links}>
             <a href="">Não tem conta?</a>
